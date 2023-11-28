f32cf6b4b725ae050c5dadec946a20a0b15e3dfe
Anastasiia <bardieranastasi@gmail.com>
2023-11-28 16:51:55 +0200

    first commit

---
+++ src/components/app/app.js
@@ -0,0 +1,31 @@
+import AppInfo from "../app-info/app-info";
+import SearchPanel from "../search-panel/search-panel";
+import AppFilter from "../app-filter/app-filter";
+import EmployeesList from "../employees-list/employees-list";
+import EmployeesAddForm from "../employees-add-form/employees-add-form";
+
+import "./app.css";
+
+function App() {
+  const data = [
+    { name: "Maria G.", salary: 800, increase: false },
+    { name: "Anna L.", salary: 3000, increase: true },
+    { name: "Diana S.", salary: 1500, increase: false },
+  ];
+
+  return (
+    <div className="app">
+      <AppInfo />
+
+      <div className="search-panel">
+        <SearchPanel />
+        <AppFilter />
+      </div>
+
+      <EmployeesList data={data} />
+      <EmployeesAddForm />
+    </div>
+  );
+}
+
+export default App;
